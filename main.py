873ccc5630d433a9633ba969d9f8cbc963463440
Sam Gledhill <samgledhill227@gmail.com>
2026-02-22 20:35:00 +0000

    refactored into OOP

--- main.py
+++ main.py
@@ -6,78 +6,73 @@ This is a quick example of integrating a video into a tkinter project
 
 import tkinter as tk
 
-from numpy import pad
 from pyvidplayer2 import VideoTkinter
 
-video = VideoTkinter("selling_everything_scaled.mp4")
+
+class App(tk.Tk):
+    def __init__(self, vidname):
+        super().__init__()
+        self.video = VideoTkinter(vidname)
+
+        self.title("Clip Manager")
+
+        video_tkframe = tk.Frame(self)
+
+        self.canvas = tk.Canvas(
+            video_tkframe,
+            width=self.video.current_size[0],
+            height=self.video.current_size[1],
+            highlightthickness=0,
+        )
+        self.canvas.pack()
+        video_tkframe.pack()
+
+        button_tkframe = tk.Frame(self)
+        gcf_button = tk.Button(button_tkframe, text="Hello", command=self.get_timestamp)
+        gcf_button.pack(pady=5)
+
+        self.seekervar = tk.DoubleVar()
+        seeker = tk.Scale(
+            button_tkframe,
+            variable=self.seekervar,
+            from_=0,
+            to=self.video.duration,
+            orient=tk.HORIZONTAL,
+            length=self.video.current_size[0] - 20,
+            tickinterval=10,
+            command=self.seek,
+        )
+        seeker.pack(pady=5)
+
+        button_tkframe.pack()
+
+        self.update_video()
+
+    def get_timestamp(self) -> float:
+        print(self.video.get_pos())
+        return self.video.get_pos()
+
+    def seek(self, e):
+        self.video.pause()
+        self.video.seek(int(e), relative=False)
+        self.video.resume()
+
+    def update_video(self):
+        self.video.draw(
+            self.canvas,
+            (self.video.current_size[0] // 2, self.video.current_size[1] // 2),
+            force_draw=False,
+        )
+
+        self.seekervar.set(self.video.get_pos())
+
+        # Designed for if video ends. Might trigger on pause?
+        if not self.video.active:
+            self.video.restart()
+
+        self.after(32, self.update_video)
 
 
-def get_timestamp() -> float:
-    print(video.get_pos())
-    return video.get_pos()
-
-
-root = tk.Tk()
-root.title("Clip Manager")
-
-video_frame = tk.Frame(root)
-button_frame = tk.Frame(root)
-
-
-canvas = tk.Canvas(
-    video_frame,
-    width=video.current_size[0],
-    height=video.current_size[1],
-    highlightthickness=0,
-)
-canvas.pack()
-video_frame.pack()
-
-gcf_button = tk.Button(button_frame, text="Hello", command=get_timestamp)
-gcf_button.pack()
-
-w_var = tk.DoubleVar()
-
-
-def seek(e):
-    video.pause()
-    video.seek(int(e), relative=False)
-    video.resume()
-
-
-w = tk.Scale(
-    button_frame,
-    variable=w_var,
-    from_=0,
-    to=video.duration,
-    orient=tk.HORIZONTAL,
-    length=video.current_size[0] - 20,
-    tickinterval=10,
-    command=seek,
-)
-w.pack(pady=5)
-
-button_frame.pack(pady=10, padx=10, side=tk.LEFT)
-
-
-def update():
-    video.draw(
-        canvas,
-        (video.current_size[0] // 2, video.current_size[1] // 2),
-        force_draw=False,
-    )
-
-    w_var.set(video.get_pos())
-
-    if video.active:
-        root.after(32, update)  # for around 60 fps
-    else:
-        pass
-        video.restart()
-        video.resume()
-
-
-update()
-root.mainloop()
-
-video.close()
+app = App("selling_everything_scaled.mp4")
+app.mainloop()
+app.video.close()
